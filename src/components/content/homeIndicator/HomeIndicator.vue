736b354705bd1ff49658c3dbcf8128131511c9d0
banshang <maoxiangtian@gmail.com>
2020-07-08 18:48:29 +0800

    7.8 仍然有问题，不能滑动，indicator无法显示

---
+++ src/components/content/homeIndicator/HomeIndicator.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+  export default {
+    name: 'HomeIndicator'
+  }
+</script>
+
+<style scoped></style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,46 +1,48 @@
 <template>
-	<div id="home">
-		<nav-bar class="home-nav">
-			<template v-slot:center>
-				<div>
-					购物街
-				</div>
-			</template>
-		</nav-bar>
-	</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <div>
+          购物街
+        </div>
+      </template>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import { getHomeMultidata } from 'network/home'
-export default {
-	name: 'Home',
-	data() {
-		return {
-			// result: null //用于保存数据
-			banners: [],
-			recommends: [],
-			
-		}
-	},
-	components: {
-		NavBar
-	},
-	created() {
-		// 1.请求多个数据
-		getHomeMultidata().then(res => {
-			console.log(res)
-			this.banners = res.data.banner.list //res指向对象 函数执行完之后，变量res会被回收 如果不再有引用，会触发垃圾回收，将返回的对象回收
-												 // 上面的赋值将res指向的对象的地址赋值给了result，则result指向对象，结果对象仍被引用，所以不会被回收
-			this.recommends = res.data.recommend.list
-		})
-	}
-}
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+
+  import { getHomeMultidata } from 'network/home'
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        // result: null //用于保存数据
+        banners: [],
+        recommends: []
+      }
+    },
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    created() {
+      // 1.请求多个数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list //res指向对象 函数执行完之后，变量res会被回收 如果不再有引用，会触发垃圾回收，将返回的对象回收
+        // 上面的赋值将res指向的对象的地址赋值给了result，则result指向对象，结果对象仍被引用，所以不会被回收
+        this.recommends = res.data.recommend.list
+      })
+    }
+  }
 </script>
 
 <style scoped>
-.home-nav {
-	background-color: var(--color-tint);
-	color: aliceblue;
-}
+  .home-nav {
+    background-color: var(--color-tint);
+    color: aliceblue;
+  }
 </style>
